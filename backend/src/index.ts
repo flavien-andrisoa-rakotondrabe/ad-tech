fb6f2850be1961b93363233e9a3a3966fd0b59db
flavienrak <flavien.andrisoarak@gmail.com>
2026-03-11 17:58:54 +0300

    fix(server): add express.json4

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,6 +31,7 @@ app.use(
 );
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(compression());
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
 
