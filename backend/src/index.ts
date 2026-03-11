b38aca4fa242afdd722ee65883f52b74d5acd2d9
flavienrak <flavien.andrisoarak@gmail.com>
2026-03-11 17:44:40 +0300

    feat(backend): update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,8 +31,6 @@ app.use(
 );
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(compression());
-app.set("trust proxy", 1);
-app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
 
